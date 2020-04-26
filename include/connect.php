aacb56dd582cba8787db59f93aca9bb34854b900
thunwa <59286787+thunwapatt@users.noreply.github.com>
2020-04-26 21:35:32 +0700

    Add files via upload

--- include/connect.php
+++ include/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-    $host="ec2-34-225-82-212.compute-1.amazonaws.com";
-    $db_name="dej6stj828jelh";
-    $user="sopcbruewylrqe";
-    $pass="709ebd6df369d5ddf480bd20e02bf16ddc36f61afe3aaaedb626aea1f82f02ba";
+    $host="ec2-18-215-99-63.compute-1.amazonaws.com";
+    $db_name="d5m65hlqagnf69";
+    $user="uyhrpiqiopccam";
+    $pass="0289bcad32487f675316a9710b19c7a12ee519da898e083248bb606fcd2b6244";
 
     $connection = pg_connect("host=$host dbname=$db_name user=$user password=$pass");
 
